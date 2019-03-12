4487cfc2be2e445ce796b93560d5523763e32db6
Padmanabhan Komanduru <pkomandu@codeaurora.org>
2019-03-12 12:40:25 +0530

    Gralloc2: Mark some gralloc bits as valid
    - Add GRALLOC_USAGE_PRIVATE_WFD and GRALLOC_USAGE_PRIVATE_10BIT_TP
    to list of valid gralloc flags.
    
    Change-Id: Icd583e7a368d06ab15d2c1845b2dca7afda69429
    CRs-Fixed: 2205241
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- libs/ui/Gralloc2.cpp
+++ libs/ui/Gralloc2.cpp
@@ -48,6 +48,8 @@ uint64_t getValid10UsageBits() {
         for (const auto bit : hardware::hidl_enum_range<BufferUsage>()) {
             bits = bits | bit;
         }
+        // TODO(b/72323293, b/72703005): Remove these invalid bits from callers
+        bits = bits | ((1 << 10) | (1 << 13) | (1 << 21) | (1 << 27));
 
 #ifdef ADDNL_GRALLOC_10_USAGE_BITS
         uint64_t addnl_bits = static_cast<uint64_t>(ADDNL_GRALLOC_10_USAGE_BITS);
