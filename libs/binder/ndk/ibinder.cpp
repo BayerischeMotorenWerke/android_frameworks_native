littleGnAl/mirror_platform_frameworks_native

Allow AIBinder<->IBinder when sharing libbinder.
FIXME: docs + test

Bug: N/A
Test: N/A
Change-Id: I4051e32e487e92ee70f85a733877cd709fc510eb

libs/binder/ndk/ibinder.cpp
@@ -15,6 +15,7 @@
  */
 
 #include <android/binder_ibinder.h>
+#include <android/binder_ibinder_platform.h>
 #include "ibinder_internal.h"
 
 #include <android/binder_stability.h>
@@ -675,4 +676,14 @@ binder_status_t AIBinder_setExtension(AIBinder* binder, AIBinder* ext) {
 
     rawBinder->setExtension(ext->getBinder());
     return STATUS_OK;
+}
+
+android::sp<android::IBinder> AIBinder_toPlatformBinder(AIBinder* binder) {
+    if (binder == nullptr) return nullptr;
+    return binder->getBinder();
+}
+AIBinder* PlatformBinder_toAIBinder(const android::sp<android::IBinder>& binder) {
+    sp<AIBinder> ndkBinder = ABpBinder::lookupOrCreateFromBinder(binder);
+    AIBinder_incStrong(ndkBinder.get());
+    return ndkBinder.get();
 }

BayerischeMotorenWerke/android_frameworks_native

Fixes: 159372351
Test: libbinder_ndk_unit_test
Signed-off-by: Akash <akashniki@gmail.com>
Signed-off-by: DennySPb <dennyspb@gmail.com>

libs/binder/ndk/ibinder.cpp
@@ -692,3 +692,14 @@ void AIBinder_setRequestingSid(AIBinder* binder, bool requestingSid) {
 const char* AIBinder_getCallingSid() {
     return ::android::IPCThreadState::self()->getCallingSid();
 }
+
+android::sp<android::IBinder> AIBinder_toPlatformBinder(AIBinder* binder) {
+    if (binder == nullptr) return nullptr;
+    return binder->getBinder();
+}
+
+AIBinder* AIBinder_fromPlatformBinder(const android::sp<android::IBinder>& binder) {
+    sp<AIBinder> ndkBinder = ABpBinder::lookupOrCreateFromBinder(binder);
+    AIBinder_incStrong(ndkBinder.get());
+    return ndkBinder.get();
+}